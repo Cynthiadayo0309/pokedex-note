72e277b0736a98a7ac2f25403dd7a8dbb4b22f28
kosukeAoki <teyontt0309@gmail.com>
2021-10-11 17:38:19 +0900

    update

--- src/views/Addresses.vue
+++ src/views/Addresses.vue
@@ -6,15 +6,18 @@
       </v-flex>
 
       <v-flex xs12 mt-5 justify-center>
-          <router-link :to="{ name: 'address_edit' }">
-              <v-btn color="info">
-                  育成メモ追加
-              </v-btn>
-          </router-link>
+        <router-link :to="{ name: 'address_edit' }">
+          <v-btn color="info">
+            育成メモ追加
+          </v-btn>
+        </router-link>
       </v-flex>
 
       <v-flex xs12 mt-5 justify-center>
-        <v-data-table :headers='headers' :items='addresses'>
+        <v-data-table 
+          :headers="headers"
+          :items="addresses"
+        >
           <template v-slot:items="props">
             <td class="text-xs-left">{{ props.item.name }}</td>
             <td class="text-xs-left">{{ props.item.cha }}</td>
@@ -23,14 +26,19 @@
             <td class="text-xs-left">{{ props.item.abi }}</td>
             <td class="text-xs-left">{{ props.item.remarks }}</td>
             <td class="text-xs-left">
-                <span>
-                    <router-link :to="{ name: 'address_edit', params: { address_id: props.item.id }}">
-                        <v-icon small class="mr-2">edit</v-icon>
-                    </router-link>
-                </span>
-                <span>
-                    <v-icon small class="mr-2" @click="deleteConfirm(props.item.id)">delete</v-icon>
-                </span>
+              <span>
+                <router-link
+                  :to="{
+                    name: 'address_edit',
+                    params: { address_id: props.item.id },
+                  }"
+                >
+                  <v-icon small class="mr-2">edit</v-icon>
+                </router-link>
+              </span>
+              <span>
+                <v-icon small class="mr-2" @click="deleteConfirm(props.item.id)">delete</v-icon>
+              </span>
             </td>
           </template>
         </v-data-table>
@@ -40,60 +48,60 @@
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapActions } from "vuex";
 
 export default {
-    created () {
-        this.addresses = this.$store.state.addresses
+  created() {
+    this.addresses = this.$store.state.addresses;
+  },
+  data() {
+    return {
+      headers: [
+        { text: "名前", value: "name" },
+        { text: "性格", value: "cha" },
+        { text: "わざ", value: "atk" },
+        { text: "アイテム", value: "item" },
+        { text: "特性", value: "abi" },
+        { text: "努力値・備考", value: "remarks" },
+        { text: "操作", sortable: false },
+      ],
+      addresses: [],
+    };
+  },
+  methods: {
+    deleteConfirm(id) {
+      if (confirm("削除してもよろしいですか？")) {
+        this.deleteAddress({ id });
+      }
     },
-    data () {
-        return {
-        headers: [
-            { text: '名前', value: 'name' },
-            { text: '性格', value: 'cha' },
-            { text: 'わざ', value: 'atk' },
-            { text: 'アイテム', value: 'item' },
-            { text: '特性', value: 'abi' },
-            { text: '努力値・備考', value: 'remarks' },
-            { text: '操作', sortable: false }
-        ],
-        addresses: []
-        }
-    },
-    methods: {
-        deleteConfirm (id) {
-            if (confirm('削除してもよろしいですか？')) {
-                this.deleteAddress({ id })
-            }
-        },
-        ...mapActions(['deleteAddress'])
-    }
-}
+    ...mapActions(["deleteAddress"]),
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 a {
-    text-decoration: none;
+  text-decoration: none;
 }
 </style>
 
 <style lang="scss">
 .container {
-    padding: 24px 0;
+  padding: 24px 0;
 }
 
 table.v-table tbody td,
 table.v-table tbody th {
-    height: 60px;
+  height: 60px;
 }
 
-// table.v-table thead td:not(:nth-child(1)), 
-// table.v-table tbody td:not(:nth-child(1)), 
-// table.v-table thead th:not(:nth-child(1)), 
-// table.v-table tbody th:not(:nth-child(1)), 
-// table.v-table thead td:first-child, 
-// table.v-table tbody td:first-child, 
-// table.v-table thead th:first-child, 
+// table.v-table thead td:not(:nth-child(1)),
+// table.v-table tbody td:not(:nth-child(1)),
+// table.v-table thead th:not(:nth-child(1)),
+// table.v-table tbody th:not(:nth-child(1)),
+// table.v-table thead td:first-child,
+// table.v-table tbody td:first-child,
+// table.v-table thead th:first-child,
 // table.v-table tbody th:first-child {
 //     padding: 0 20px;
 // }
--- src/views/VeeValidate.vue
+++ src/views/VeeValidate.vue
@@ -1,28 +1,28 @@
 <template>
-    <div style="text-align: center;">
-        <h1>VeeValidate</h1>
+  <div style="text-align: center;">
+    <h1>VeeValidate</h1>
 
-        <form @submit.prevent="onSubmit">
-            <label>お名前</label>
-            <ValidationProvider name="お名前" rules="required" v-slot="{ errors }">
-                <div>
-                    <input type="text" name="name" v-model="form.name">
-                </div>
-                <div class="text-danger">{{ errors[0] }}</div>
-            </ValidationProvider>
-
-            <label>メールアドレス</label>
-            <ValidationProvider name="メールアドレス" rules="required|email" v-slot="{ errors }">
-                <div>
-                    <input type="email" name="email" v-model="form.email">
-                </div>
-                <div class="text-danger">{{ errors[0] }}</div>
-            </ValidationProvider>
-
-            <div class="mt-2">
-                <button type="submit">送信</button>
+      <form @submit.prevent="onSubmit">
+        <label>お名前</label>
+          <ValidationProvider name="お名前" rules="required" v-slot="{ errors }">
+            <div>
+              <input type="text" name="name" v-model="form.name">
             </div>
-        </form>
+            <div class="text-danger">{{ errors[0] }}</div>
+          </ValidationProvider>
+
+        <label>メールアドレス</label>
+          <ValidationProvider name="メールアドレス" rules="required|email" v-slot="{ errors }">
+            <div>
+              <input type="email" name="email" v-model="form.email">
+            </div>
+            <div class="text-danger">{{ errors[0] }}</div>
+          </ValidationProvider>
+
+          <div class="mt-2">
+            <button type="submit">送信</button>
+          </div>
+      </form>
     </div>
 </template>
 
